1e7f9874dce758433690dc1c0e688e480f495743
Fredz66 <frederic.lopez@free.fr>
2019-06-05 02:23:22 +0200

    v0.4

--- haxeflixel-game.js
+++ haxeflixel-game.js
@@ -70535,7 +70535,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 524786;
+	this.version = 309490;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
