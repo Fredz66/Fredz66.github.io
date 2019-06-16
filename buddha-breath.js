ee590eaaabac4bd3318cd1c277fa1ec2c79f5509
Fredz66 <frederic.lopez@free.fr>
2019-06-17 00:52:21 +0200

    v0.4

--- buddha-breath.js
+++ buddha-breath.js
@@ -894,9 +894,9 @@ ApplicationMain.create = function(config) {
 	ManifestResources.init(config);
 	var _this = app.meta;
 	if(__map_reserved["build"] != null) {
-		_this.setReserved("build","85");
+		_this.setReserved("build","87");
 	} else {
-		_this.h["build"] = "85";
+		_this.h["build"] = "87";
 	}
 	var _this1 = app.meta;
 	if(__map_reserved["company"] != null) {
@@ -71363,7 +71363,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 54486;
+	this.version = 268940;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
@@ -116569,7 +116569,7 @@ openfl_display_DisplayObject.__tempStack = new lime_utils_ObjectPool(function() 
 	stack.set_length(0);
 });
 Main.level = 1;
-Main.version = "v2.1.0-alpha";
+Main.version = "v2.2.0-alpha";
 flixel_math_FlxRect._pool = new flixel_util_FlxPool_$flixel_$math_$FlxRect(flixel_math_FlxRect);
 flixel_FlxObject.defaultPixelPerfectPosition = false;
 flixel_FlxObject.SEPARATE_BIAS = 4;
